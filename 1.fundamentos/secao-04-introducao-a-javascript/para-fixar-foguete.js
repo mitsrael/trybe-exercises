cc3e8070031d349d8341c8e378e2dfab8c5f6ca2
mitsrael <mitssrael@gmail.com>
2023-01-21 13:29:01 -0300

    Exercicio do foguete

---
+++ 1.fundamentos/secao-04-introducao-a-javascript/para-fixar-foguete.js
@@ -0,0 +1,10 @@
+const myName = "Mits";
+const birthCity = "Estreito";
+let birthYear = 1999;
+birthYear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+/* não é possivel reatribuir valor para uma const. */
