69fd35db412f61ff3c3e84f54a0866543d08f7b9
mitsrael <mitssrael@gmail.com>
2023-01-22 12:26:03 -0300

    Exercicio do foguete 3

---
+++ 1.fundamentos/secao-04-introducao-a-javascript/para-fixar-foguete-3.js
@@ -0,0 +1,9 @@
+const note = 85;
+
+if (note >= 80) {
+    console.log("Parabéns, você faz parte do grupo das pessoas aprovadas!");
+} else if (note < 80 && note >= 60) {
+    console.log("Você está na nossa lista de espera");
+} else if (note < 60) {
+    console.log("Infelizmente, você reprovou.");
+}
