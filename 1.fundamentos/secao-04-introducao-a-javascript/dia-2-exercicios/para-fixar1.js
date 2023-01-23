5fa0839c6ec4514b87a34b25ba2f8b76aeea38ff
mitsrael <mitssrael@gmail.com>
2023-01-23 19:25:24 -0300

    Commit

--- 1.fundamentos/secao-04-introducao-a-javascript/dia-2-exercicios/para-fixar1.js
+++ 1.fundamentos/secao-04-introducao-a-javascript/dia-2-exercicios/para-fixar1.js
@@ -1,14 +1,7 @@
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu[1];
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+groceryList.push('Frango')
+groceryList.unshift('Banana')
 
-console.log(menuServices);
-
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let indexOfPortfolio = menu.indexOf('Portfólio');
-
-console.log(indexOfPortfolio);
-
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push('Contato');
-
-console.log(menu);
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}
